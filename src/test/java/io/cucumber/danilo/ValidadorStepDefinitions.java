c8a0e89e9a67e04b8910b135a86078297177e2a0
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-03-15 10:15:01 -0300

    cucumber com exemplos sleep

--- src/test/java/io/cucumber/danilo/ValidadorStepDefinitions.java
+++ src/test/java/io/cucumber/danilo/ValidadorStepDefinitions.java
@@ -15,6 +15,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ValidadorStepDefinitions {
 
     public WebDriver driver;
@@ -24,14 +26,15 @@ public class ValidadorStepDefinitions {
         // System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://localhost:8081/validar-cpf");
-        driver.manage().timeouts().implicitlyWait(300,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
     }
 
     @When("I fill the name")
-    public void i_fill_the_name() {
+    public void i_fill_the_name() throws InterruptedException {
         WebElement campoNome = driver.findElement(By.name("nome"));
         campoNome.clear();
         campoNome.sendKeys("Danilo");
+        Thread.sleep(2000);
     }
 
     @When("I fill de CPF {string}")
@@ -43,14 +46,25 @@ public class ValidadorStepDefinitions {
     }
     
     @Then("A have a invalid CPF")
-    public void a_have_a_invalid_CPF() {
+    public void a_have_a_invalid_CPF() throws InterruptedException {
+        // WebDriverWait wait = new WebDriverWait(driver, 10);
+        // wait.until(ExpectedConditions.visibilityOf(camponome));
+        
         WebElement success = driver.findElement(By.className("alert-danger"));
+        Thread.sleep(2000);
         driver.quit();
     }
 
     @Then("A have a valid CPF")
-    public void a_have_a_valid_CPF() {
+    public void a_have_a_valid_CPF() throws InterruptedException {
         WebElement success = driver.findElement(By.className("alert-success"));
+
+        String mensagem = driver.findElement(By.className("alert-success")).getText();
+        // String mensagem = driver.findElement(By.id("enviar")).getText();
+        // String mensagem = driver.findElement(By.tagName("div")).getText();
+        assertEquals(mensagem , "foi validada como válido");
+
+        Thread.sleep(2000);
         driver.quit();
     }
 }
